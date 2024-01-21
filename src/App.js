857ebcdd01a8a7a602560d8f74d8d212a09ed3d3
jeffng5 <jeffreykng@hotmail.com>
2024-01-21 16:20:30 -0500

    fixed loading component

--- src/App.js
+++ src/App.js
@@ -2,56 +2,52 @@ import { useEffect, useState } from "react";
 import JokeList from "./JokeList"
 import { FidgetSpinner } from 'react-loader-spinner'
 import Joke from "./Joke"
-import Popup from "reactjs-popup";
+
 import "./App.css"
 
 function App() {
+ 
  const [state, setState]= useState(true)
 
  useEffect(() => {
     setInterval(() => { 
       setState(false)
-    }, 5000);
-  },[])
+    }, 3000);
+  },[state])
 // function ActionLink() {
 //   const handleClick = (evt) => {evt.preventDefault();
 //    console.log("This is being updated") }
 //   }
 
+if (state===true) {
+  return (
+  <body>
+  <FidgetSpinner className="fidget" height="380" width= "380"/><h3 className= "backgroundLoading">Loading...</h3>
 
-return (
-        <div className="App">
-<div>
-<Popup trigger={<button className="trigger">Load</button>} position="right center">
-<div className= 'fidget'><FidgetSpinner
-  visible= {true}
-  height="380"
-  width="380"
-  ariaLabel="fidget-spinner-loading"
-  wrapperStyle={{}}
-  wrapperClass="fidget-spinner-wrapper"
-  /><h3>Loading...</h3></div>
-  </Popup>                
-  </div>        
+</body>
+  )
 
+ }     
+  
 
+if (state===false) {
+    return (
+      <>
+      <div className="App">
 <h1>Joke Generator</h1>
                 <div><JokeList/></div><Joke />
                 <div><JokeList/></div><Joke />
                 <div><JokeList/></div><Joke />
                 <div><JokeList/></div><Joke />
                 <div><JokeList/></div><Joke />
-
+</div>
   
-;
-        </div>
-       
-    );
+ </>
+    )      
+    ;
 }
 
-
-
-
+}
 
 
 
--- src/Joke.js
+++ src/Joke.js
@@ -5,8 +5,8 @@ import "./Joke.css";
 /** A single joke, along with vote up/down buttons. */
 
 function Joke({id, vote, votes, text}) {
-  const [count, setCount] = useState(1)
-  const [count1, setCount1] = useState(1)
+  const [count, setCount] = useState(0)
+  const [count1, setCount1] = useState(0)
   
     function increment() {
       setCount(count+1);
@@ -23,18 +23,20 @@ function Joke({id, vote, votes, text}) {
     return (
       <div className="Joke">
         <div className="Joke-votearea">
-        
+          <span>       
           <button onClick={() => increment()}>
             <i className="fas-fa-thumbs-up" />
           </button>
-          <h4>{count}</h4>
-          
+          <h4 className="h4">{count}</h4>
+          </span>
+          <span>
           <button onClick={()=> increment1()}>
             <i className="fas fa-thumbs-down" />
           </button>
-          <h4>{count1}</h4>
-      
+          <h4 className="h4">{count1}</h4>
+        </span>
           {votes}
+        
         </div>
 
         <div className="Joke-text">{text}</div>
